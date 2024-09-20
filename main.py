4718c4d3fb36a2e7487963ff3819b5e5e30c0cb1
NavyaM94 <navyam0206@gmail.com>
2024-09-19 23:09:48 -0400

    lib system from local

---
+++ main.py
@@ -0,0 +1,9 @@
+from pythonCode.models import Book,User
+
+if __name__ == "__main__":
+    book1= Book(title="testtitle1",author="testAuthor1",genre="testGenre1")
+    book2= Book(title="testtitle2",author="testAuthor2",genre="testGenre2", is_borrowed=True)
+
+    print(book1)
+    print(book2)
+    
